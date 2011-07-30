0c658eb75b67e05baa02956732f57055665468d9
Matthew Johnston <mjohnston1991@att.net>
2011-07-30 01:40:53 -0500

    Doesn't work with / for some reason. Looking into the issue.

--- application/routes/home.php
+++ application/routes/home.php
@@ -1,12 +1,18 @@
 <?php
 
+/*
+ * For some reason this doesn't want to work
+ * when it comes to the index page. Looking
+ * into the reason.
+ */
 return array(
 
 	'GET /' => function()
 	{
 		$user = User::find(Session::get('laravel_user_id'));
 		
+		print_r(array('testing', 'tester'));
 		return View::make('home/index')->partial('header', 'partials/header2col')
-									   ->partial('footer', 'partials/footer2col');
+									   ->partial('footer', 'partials/footer2col', array('user' => $user));
 	}
 );
